8a0dda625fbf0345212a79b55af2ae92bf73a923
Decagon <decagon@Decagons-MacBook-Air.local>
2022-03-02 15:02:29 +0100

    assignment done

--- src/analysis.js
+++ src/analysis.js
@@ -1,4 +1,5 @@
 const { getTrips } = require('api');
+const drivers = require('api/data/drivers')
 
 /**
  * This function should return the trip data analysis
@@ -8,6 +9,76 @@ const { getTrips } = require('api');
  */
 async function analysis() {
   // Your code goes here
-}
+  //get all trips
+  let driversIds = Object.keys(drivers);
+ 
+//expected outputs
+let numberOfDriversWithMoreThanOneVehicle= 0;
+let highestTripsByDriver =0;
+let numberOfTripsByHighestEarningDriver = 0;
+const allTrips = await getTrips();
+let driverWithTheMostTrips;
+let  mostTripDriverID;
+let highestEarningDriver;
+let highestEarning =0;
 
+for(const driverId of driversIds) {
+  let currentDriver = drivers[driverId]
+
+  //get trips by driver
+  let tripsByDriver = allTrips.filter(trip => trip.driverID === driverId).length;
+  //get the earning of each driver
+  let earnings = parseFloat(allTrips.filter(trip => trip.driverID === driverId).map(trip=> parseFloat(`${trip.billedAmount || ''}`.replace(',',''))).reduce((a,b) => a+b,0).toFixed(2));
+
+  //set condition to get the highest earning, highest earning driver naumber of trips by the highest earning driver
+  if(earnings >= highestEarning){
+    highestEarning = earnings;
+    highestEarningDriver = currentDriver;
+    numberOfTripsByHighestEarningDriver = tripsByDriver;
+  }
+
+   //set conditions to get the highest tripd by driver, driver with the most trip and his id
+   if(tripsByDriver > highestTripsByDriver) {
+    highestTripsByDriver = tripsByDriver;
+    driverWithTheMostTrips = currentDriver;
+    mostTripDriverID = driverId;
+   }
+   if (currentDriver.vehicleID.length > 1) numberOfDriversWithMoreThanOneVehicle++; 
+}
+//get the cash trips
+let cashTrips = allTrips.filter(trip => trip.isCash === true);
+//get the non cash trips
+let nonCashTrips = allTrips.filter(trip => trip.isCash === false);
+//get the total cash bill
+const cashBilledTotal = cashTrips.map(trip=> parseFloat(`${trip.billedAmount || ''}`.replace(',',''))).reduce((a,b) => a+b,0)
+//get the total non cash bill
+const nonCashBilledTotal = parseFloat(nonCashTrips.map(trip=> parseFloat(`${trip.billedAmount || ''}`.replace(',',''))).reduce((a,b) => a+b,0).toFixed(2));
+
+//get all result object
+result = {
+  "noOfCashTrips": cashTrips.length,
+  "noOfNonCashTrips": nonCashTrips.length,
+  "billedTotal": cashBilledTotal + nonCashBilledTotal,
+  "cashBilledTotal": cashBilledTotal,
+  "nonCashBilledTotal": nonCashBilledTotal,
+  "noOfDriversWithMoreThanOneVehicle": numberOfDriversWithMoreThanOneVehicle-1,
+  "mostTripsByDriver": {
+    "name": driverWithTheMostTrips.name,
+    "email": driverWithTheMostTrips.email,
+    "phone": driverWithTheMostTrips.phone,
+    "noOfTrips": highestTripsByDriver,
+    "totalAmountEarned": allTrips.filter(trip => trip.driverID === mostTripDriverID).map(trip=> parseFloat(`${trip.billedAmount || ''}`.replace(',',''))).reduce((a,b) => a+b,0)
+  },
+  "highestEarningDriver": {
+    "name": highestEarningDriver.name,
+    "email": highestEarningDriver.email,
+    "phone": highestEarningDriver.phone,
+    "noOfTrips": numberOfTripsByHighestEarningDriver,
+    "totalAmountEarned": highestEarning
+  }
+}
+return result;
+}
 module.exports = analysis;
+analysis()
+
--- src/report.js
+++ src/report.js
@@ -1,14 +1,61 @@
-const { getTrips } = require('api');
+const { getTrips, getDriver, getVehicle } = require('api');
+const drivers = require('api/data/drivers');
 
 /**
  * This function should return the data for drivers in the specified format
  *
  * Question 4
  *
- * @returns {any} Driver report data
+\ * @returns {any} Driver report data
  */
 async function driverReport() {
   // Your code goes here
+  driversIds = Object.keys(drivers);
+  let report = [];
+  const allTrips = await getTrips();
+  let trips = [...allTrips];
+ for(const driver of driversIds){
+   let tripsByDriver = trips.filter(trip => trip.driverID === driver);
+   let cashTripsByDriver = allTrips.filter(trip => trip.isCash === true && trip.driverID === driver);
+   let noneCashTripsByDriver = allTrips.filter(trip => trip.isCash === false && trip.driverID === driver);
+  let driverInfo = await getDriver(driver)
+  let noOfVehicles = driverInfo.vehicleID.map(vehicle => getVehicle(vehicle));
+  let resolvedNoOfVehicles = (await Promise.all(noOfVehicles)).map(vehicle => ({
+    "plate": vehicle.plate,
+    "manufacturer": vehicle.manufacturer
+  }));
+  report.push({
+    "fullName": driverInfo.name,
+    "id": driver,
+    "phone": driverInfo.phone,
+    "noOfTrips": tripsByDriver.length,
+    "noOfVehicles": noOfVehicles.length,
+    "vehicles": resolvedNoOfVehicles,
+    "noOfCashTrips": cashTripsByDriver.length,
+    "noOfNonCashTrips": noneCashTripsByDriver.length,
+    "totalAmountEarned": cashTripsByDriver.map(trip=> parseFloat(`${trip.billedAmount || ''}`.replace(',',''))).reduce((a,b) => a+b,0)
+    +
+    noneCashTripsByDriver.map(trip=> parseFloat(`${trip.billedAmount || ''}`.replace(',',''))).reduce((a,b) => a+b,0)
+    ,
+    "totalCashAmount": cashTripsByDriver.map(trip=> parseFloat(`${trip.billedAmount || ''}`.replace(',',''))).reduce((a,b) => a+b,0),
+    "totalNonCashAmount": noneCashTripsByDriver.map(trip=> parseFloat(`${trip.billedAmount || ''}`.replace(',',''))).reduce((a,b) => a+b,0),
+    "trips": tripsByDriver.map(trip=>(
+      {
+            "user": trip.user.name,
+            "created": trip.created,
+            "pickup": trip.pickup.address,
+            "destination": trip.destination.address,
+            "billed": trip.billedAmount,
+            "isCash": trip.isCash
+      }
+    ))
+  })
 }
+//  report
+//  console.log(report);
+ return report;
+
+}
+driverReport()
 
 module.exports = driverReport;
